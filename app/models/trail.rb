f74bbcc094133c2eb69c156df13bda838ea93ec9
grosser <grosser.michael@gmail.com>
2011-06-22 14:44:49 +0200

    fix attribute setting

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -9,10 +9,12 @@ class Trail
   def self.track!(options)
     raise unless options[:path]
 
-    attributes = Hash[options.select{|k,v| k.starts_with?('data-')}.map{|k,v| [k.sub('data-',''),v] }]
+    attributes = Hash[options.select{|k,v| k.to_s.starts_with?('data-')}.map{|k,v| [k.sub('data-',''),v] }]
 
-    unless options[:id] and trail = where(:created_at.gt => TIMEOUT.ago).find_by_id(options[:id])
-      trail = create!
+    if options[:id] and trail = where(:created_at.gt => TIMEOUT.ago).find_by_id(options[:id])
+      trail.update_attributes(attributes)
+    else
+      trail = create!(attributes)
       trail.visits.create!(:path => options[:referrer]) if options[:referrer].present?
     end
     trail.visits.create!(:path => options[:path])
