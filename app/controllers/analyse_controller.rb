0c2db9ae69b6212a68d199f132649346ed86e8e3
grosser <grosser.michael@gmail.com>
2011-06-24 16:55:05 +0200

    better filtering

--- app/controllers/analyse_controller.rb
+++ app/controllers/analyse_controller.rb
@@ -5,16 +5,16 @@ class AnalyseController < ApplicationController
     @tags = Trail.where.distinct(:tags).sort
 
     all_paths = (params[:paths]||{}).select{|k,v|v.present?}.sort.map(&:last)
-    compare = (params[:compare]||{}).select{|k,v|v.present?}.sort.map(&:last)
+    @compare = (params[:compare]||{}).select{|k,v|v.present?}.sort.map(&:last)
 
-    return if all_paths.empty? or compare.empty?
+    return if all_paths.empty? or @compare.empty?
 
     paths = []
     @path_trails = all_paths.map do |path|
       paths << path
 
       scope = Trail.with_paths_in_order(paths)
-      counts = compare.map do |tag|
+      counts = @compare.map do |tag|
         if tag == 'all'
           scope.count
         else
