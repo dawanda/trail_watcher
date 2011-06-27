75ab1597299fa27579b604d101d0380667cb9902
grosser <grosser.michael@gmail.com>
2011-06-27 12:51:01 +0200

    nicer error trace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  def render_simple_error(exception)
-    render :text => "#{exception.inspect}<br/>#{exception.backtrace.join("<br/>")}"
+  def render_simple_error(e)
+    render :text => "#{e.message} -- #{e.class}<br/>#{e.backtrace.join("<br/>")}"
   end
 end
